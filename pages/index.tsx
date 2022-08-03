abfdf8fb734502d9aecbe2e5c32549a7780adac1
Yusuke Noguchi <yusuke.0726@gmail.com>
2022-08-04 07:21:54 +0900

    🐛 FIxed a bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ const Home = ({
   return (
     <div
       className={` relative h-screen bg-gradient-to-b lg:h-[140vh] 
-      ${showModal && "h-screen overflow-hidden"}`}
+      ${showModal && "!h-screen overflow-hidden"}`}
     >
       <Head>
         <title>Home - Netflix</title>
